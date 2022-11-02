3171f0549398475975d33cdaa6748c8cda245028
Lazar Nikolic <nikoliclazarln@gmail.com>
2022-11-02 10:08:21 +0100

    my first route, added styling, posts, and a post page

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/app.css">
+    <title>My Blog</title>
+</head>
+<body>
+    <article>
+        <h1><a href="/post">My first post</a></h1>
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate necessitatibus sint voluptates? Laudantium voluptatibus modi at sit nisi qui quaerat!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ea consequatur voluptate quas corporis vero officia, dicta pariatur sint, quam voluptatem officiis non totam ducimus laborum! Sapiente ab eius labore commodi excepturi provident sint consectetur, quisquam quibusdam corporis suscipit ut beatae neque repellendus explicabo fugiat? Nihil officia corporis amet quos!
+        </p>
+    </article>
+    <article>
+        <h1><a href="/post">My second post</a></h1>
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate necessitatibus sint voluptates? Laudantium voluptatibus modi at sit nisi qui quaerat!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ea consequatur voluptate quas corporis vero officia, dicta pariatur sint, quam voluptatem officiis non totam ducimus laborum! Sapiente ab eius labore commodi excepturi provident sint consectetur, quisquam quibusdam corporis suscipit ut beatae neque repellendus explicabo fugiat? Nihil officia corporis amet quos!
+        </p>
+    </article>
+    <article>
+        <h1><a href="/post">My third post</a></h1>
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate necessitatibus sint voluptates? Laudantium voluptatibus modi at sit nisi qui quaerat!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ea consequatur voluptate quas corporis vero officia, dicta pariatur sint, quam voluptatem officiis non totam ducimus laborum! Sapiente ab eius labore commodi excepturi provident sint consectetur, quisquam quibusdam corporis suscipit ut beatae neque repellendus explicabo fugiat? Nihil officia corporis amet quos!
+        </p>
+    </article>
+</body>
+</html>
