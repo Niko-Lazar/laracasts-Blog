cec4eeba2ab87adf221dc5d6dcf760683d4e300d
Lazar Nikolic <nikoliclazarln@gmail.com>
2022-11-03 14:16:57 +0100

    route model binding, and fillables

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,10 @@ Route::get('/', function () {
     return view('posts', ['posts' => Post::all()]);
 });
 
-Route::get('posts/{post}', function($id) {
+Route::get('posts/{post:slug}', function(Post $post) {
 
     return view('post', [
-        'post' => Post::findOrFail($id)
+        'post' => $post
     ]);
 
 });
