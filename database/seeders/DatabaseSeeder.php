6490af32db64de62fc739c3e4c36306f245435fe
Lazar Nikolic <nikoliclazarln@gmail.com>
2022-11-03 18:12:53 +0100

    seed factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,43 +17,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Post::truncate();
-        Category::truncate();
-
-        $user = User::factory(1)->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal',
+        $user = User::factory()->create([
+            'name' => 'Jack Sparrow'
         ]);
-
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family',
-        ]);
-
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work',
-        ]);
-
-        Post::create([
-            'user_id' => 1,
-            'category_id' => 1,
-            'title' => 'My First Family Post',
-            'slug' => 'my-first-post',
-            'excerpt' => "<p>Lorem ipsum dolor sit amet. Est nihil quae aut enim debitis et reprehenderit consequatur. A tenetur excepturi</p>",
-            'body' => "<p>Lorem ipsum dolor sit amet. Est nihil quae aut enim debitis et reprehenderit consequatur. A tenetur excepturi aut consequuntur dolorem eum consequatur quam est aliquid explicabo quo commodi nostrum sed quia commodi nam nihil quos. Et fugiat cupiditate et unde deserunt ab maiores impedit sed quia mollitia non tenetur nesciunt et eveniet excepturi ut sint maiores.Ea ipsum vitae est soluta labore est voluptates blanditiis et fuga accusamus non commodi distinctio quo internos officiis. Nam nesciunt magni ad recusandae ullam At velit quas. Qui consequatur nihil qui aperiam optio in molestiae dolorum eos itaque quia qui repellendus aperiam eum quia doloribus sit accusamus quia?</p>",
-        ]);
-
-        Post::create([
-            'user_id' => 1,
-            'category_id' => 2,
-            'title' => 'My Second Work Post',
-            'slug' => 'my-second-post',
-            'excerpt' => "<p>Lorem ipsum dolor sit amet. Est nihil quae aut enim debitis et reprehenderit consequatur. A tenetur excepturi</p>",
-            'body' => "<p>Lorem ipsum dolor sit amet. Est nihil quae aut enim debitis et reprehenderit consequatur. A tenetur excepturi aut consequuntur dolorem eum consequatur quam est aliquid explicabo quo commodi nostrum sed quia commodi nam nihil quos. Et fugiat cupiditate et unde deserunt ab maiores impedit sed quia mollitia non tenetur nesciunt et eveniet excepturi ut sint maiores.Ea ipsum vitae est soluta labore est voluptates blanditiis et fuga accusamus non commodi distinctio quo internos officiis. Nam nesciunt magni ad recusandae ullam At velit quas. Qui consequatur nihil qui aperiam optio in molestiae dolorum eos itaque quia qui repellendus aperiam eum quia doloribus sit accusamus quia?</p>",
+        Post::factory(5)->create([
+            'user_id' => $user->id,
         ]);
     }
 }
