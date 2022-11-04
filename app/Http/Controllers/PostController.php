208c5e25deb8d3b04377c03091a30c11f585cb21
Lazar Nikolic <nikoliclazarln@gmail.com>
2022-11-04 20:50:57 +0100

    added pagination when selecting specific category aswell, using withQueryString method

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,7 @@ class PostController extends Controller
     {
         return view('posts.index', [
             'posts' => Post::latest()->filter(
-                request(['search', 'category', 'author']))->simplePaginate(5),
+                request(['search', 'category', 'author']))->simplePaginate(5)->withQueryString(),
         ]);
     }
 
