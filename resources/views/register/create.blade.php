81880e45261c1a53e18678fac9398898c674b91f
Lazar Nikolic <nikoliclazarln@gmail.com>
2022-11-05 11:05:16 +0100

    added error validation messages and unique properties for fields in RegisterCotroller

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -16,8 +16,14 @@
                            type="text"
                            name="name"
                            id="name"
+                           value="{{ old('name') }}"
                            required
                     >
+
+                    @error('name')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
+
                 </div>
 
                 <div class="mb-6">
@@ -31,8 +37,13 @@
                         type="text"
                         name="username"
                         id="username"
+                           value="{{ old('username') }}"
                         required
                     >
+
+                    @error('username')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -46,6 +57,7 @@
                            type="email"
                            name="email"
                            id="email"
+                           value="{{ old('email') }}"
                            required
                     >
 
@@ -67,6 +79,10 @@
                            id="password"
                            required
                     >
+
+                    @error('password')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -76,6 +92,14 @@
                     Submit
                     </button>
                 </div>
+
+                @if($errors->any())
+                    <ul>
+                        @foreach($errors->all() as $error)
+                            <li class="text-red-500 text-xs">{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                @endif
             </form>
         </main>
     </section>
