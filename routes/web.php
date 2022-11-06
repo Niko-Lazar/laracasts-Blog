59568711212b130fe7d3057ae4b1340ac5e1aac5
Lazar Nikolic <nikoliclazarln@gmail.com>
2022-11-06 13:15:41 +0100

    newsletters first look (mailchimp)

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,19 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('ping', function() {
+
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us21'
+    ]);
+
+    $response = $mailchimp->lists->getAllLists();
+    dd($response);
+});
+
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
